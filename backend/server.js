d4fc0a483908934a48bb2e0e2e50c43648b706c6
Joey Huang <tul32032@temple.edu>
2025-10-28 09:55:50 -0400

    design db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
-const express = require('express');
+import express from "express";
+import pool from "./src/db.js";
+
 const app = express();
+app.use(express.json());
+
 const port = 5173;
 
-app.get('/', (req, res) => {
-    res.send("Argh!");
+app.get("/", (req, res) => {
+  res.send("Argh!");
+});
+
+app.post("/api/users", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING *",
+      [email, password],
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(port, () => {
-    console.log('Server is listening')
+  console.log("Server is listening");
 });
