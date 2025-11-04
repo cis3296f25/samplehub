ca890089134c0b32321a9478f209f1ea7190cd7e
Arwin-j <tuq56068@temple.edu>
2025-11-04 07:07:52 -0500

    Minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
-import userRoutes from "./routes/user";
+import userRoutes from "./routes/user.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import freesoundRoutes from "./routes/scraper.js"
 
 const port = 5173;
 
 const app = express();
-
+app.use(cors());
+app.use("/api", freesoundRoutes);
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -12,8 +16,18 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/", (req, res)=> {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5173;
+
+app.get("/api/test", (req, res) => {
+  res.send("API route is working");
+});
+
 app.use("/api/user/", userRoutes);
 
 app.listen(port, () => {
-  console.log("Server is listening");
+  console.log(`Server is listening, started at port ${PORT}`);
 });
