c4e63675999c9c1a1bff5a3ea2c8829ab273401b
Shiven Patel <shiven9019@gmail.com>
2025-11-06 10:19:43 -0500

    Home Page

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_LagvYj2O-hZrzJaiF0GgjFvzE1ACku8",
+  authDomain: "samplehub-3296.firebaseapp.com",
+  projectId: "samplehub-3296",
+  storageBucket: "samplehub-3296.firebasestorage.app",
+  messagingSenderId: "263496867495",
+  appId: "1:263496867495:web:dd22ccc94f342ff25a8582",
+  measurementId: "G-TCFGYXXJBP",
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
